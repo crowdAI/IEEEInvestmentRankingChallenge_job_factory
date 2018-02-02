dd1e4c33c2e0444ad76e5cf0a61eec277be9aff6
Sean Carroll <sfcarroll@gmail.com>
2018-02-02 11:42:58 +0100

    Changes to the overall architecture for simplicity

--- workers.py
+++ workers.py
@@ -15,53 +15,17 @@ import numpy as np
 
 import requests
 
+from evalute import _evaluate
+
 POOL = redis.ConnectionPool(host=config.redis_host, port=config.redis_port, db=0)
 
-def _evaluate(predicted_heights, true_heights, context):
-    """
-        takes a list of predicted heights and true heights and computes the score
-    """
-    score = 0
-    secondary_score = 0
-
-    """
-    for k in range(100):
-        time.sleep(random.randint(0,100)/1000.0)
-        percent_complete = k*1.0/100 * 100
-        update_progress(context, percent_complete, "")
-        # print "Context Response Channel ::: ", context['response_channel']
-        # if k%20==0:
-        #     # print "Update : ", percent_complete
-        score += random.randint(1,100)*1.0/0.7 / 100
-        secondary_score += random.randint(1,100)*1.0/0.7 / 100
-    """
-   
-    predicted_heights = np.array(predicted_heights)
-    true_heights = np.array(true_heights)
-    if predicted_heights.shape != true_heights.shape:
-        raise Exception("Wrong number of predictions provided. Expected a variable of shape %s, but received a variable of shape %s instead" % ( str(true_heights.shape), str(predicted_heights.shape) ))
-
-    score = r2_score(true_heights, predicted_heights)
-    secondary_score = mean_squared_error(true_heights, predicted_heights)
-
-    _result_object = {
-        "score" : score,
-        "score_secondary" : secondary_score,
-    }
-    return _result_object
-
-def _submit(predicted_heights, true_heights, context):
+def _submit(predicted_data_path, answer_file_path, context):
     """
         takes a list of predicted heights and actual heights and computes the score
 
         and prepares the plots for submission to the leaderboard
     """
-    #_result_object = _evaluate(predicted_heights[:50], true_heights[:50], context)
-    _result_object = _evaluate(predicted_heights, true_heights, context)
-    #_result_object["comment"] = ""
-    #_result_object["media_large"] = "https://upload.wikimedia.org/wikipedia/commons/4/44/Drift_Diffusion_Model_Accumulation_to_Threshold_Example_Graphs.png"
-    #_result_object["media_thumbnail"] = "https://upload.wikimedia.org/wikipedia/commons/4/44/Drift_Diffusion_Model_Accumulation_to_Threshold_Example_Graphs.png"
-    #_result_object["media_content_type"] = "image/jpeg"
+    _result_object = _evaluate(predicted_data_path, answer_file_path, context)
 
 
     # Upload result to crowdai
@@ -76,12 +40,12 @@ def _submit(predicted_heights, true_heights, context):
     print "Making POST request...."
     r = requests.post(config.CROWDAI_GRADER_URL, params=_payload, headers=headers, verify=False)
     print "Status Code : ",r.status_code
-    
+
     if r.status_code == 202:
         data = json.loads(r.text)
         submission_id = str(data['submission_id'])
-        _payload['data'] = predicted_heights
-        context['redis_conn'].set(config.challenge_id+"::submissions::"+submission_id, json.dumps(_payload)) 
+        _payload['data'] = predicted_data_path
+        context['redis_conn'].set(config.challenge_id+"::submissions::"+submission_id, json.dumps(_payload))
         pass #TODO: Add success message
     else:
         raise Exception(str(r.text))
@@ -115,8 +79,9 @@ def job_execution_wrapper(data):
     try:
         if data["function_name"] == "submit":
             # Run the job
-            true_heights = np.load("test_heights.npy")
-            result = _submit(data["data"], true_heights, _context)
+            answer_file_path = config.answer_file_path
+
+            result = _submit(data["data"], answer_file_path, _context)
             # Register Job Complete event
             _update_job_event(_context, job_info_template(_context, "Scores Submitted Successfully ! Coefficient of Determination(R^2) : %s ; MSE : %s" % (str(result['score']), str(result['score_secondary'])) ))
             _update_job_event(_context, job_complete_template(_context, result))
